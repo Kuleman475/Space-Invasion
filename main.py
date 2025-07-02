3a13247fa6d65dd8760b5942b19147f84fe2f6b5
Taylor Christensen <chirstensentaylor6@gmail.com>
2025-07-02 16:24:07 -0400

    game done

--- main.py
+++ main.py
@@ -21,14 +21,12 @@ ENEMY_VERTICAL_MARGIN = 15
 RIGHT_ENEMY_BORDER = SCREEN_WIDTH - ENEMY_VERTICAL_MARGIN
 LEFT_ENEMY_BORDER = ENEMY_VERTICAL_MARGIN
 
-class MyGame(arcade.Window):
+class MyGame(arcade.View):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__()
         arcade.set_background_color(arcade.color.BLACK)
 
         self.player_list = arcade.SpriteList()
-        self.player = Player()
-        self.player_list.append(self.player.player_list)
 
         self.enemy_change_x = -ENEMY_MOVEMENT_SPEED
         self.big_boss_change = -BOSS_MOVEMENT_SPEED
@@ -36,6 +34,8 @@ class MyGame(arcade.Window):
     def setup(self):
         self.enemies = Enemies()
         self.player = Player()
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player)
 
     def on_draw(self):
         self.clear()
@@ -44,6 +44,11 @@ class MyGame(arcade.Window):
             self.enemies.draw()
         self.enemies.enemy_list.draw()
         self.player.player_bullet_list.draw()
+        arcade.draw_text(
+        f"Health: {self.player.health}",
+        10, SCREEN_HEIGHT - 30,
+        arcade.color.WHITE, 20
+)
 
 
 #update each frame
@@ -62,6 +67,7 @@ class MyGame(arcade.Window):
         for bullet in self.enemies.enemy_bullet_list:
             if bullet.bottom < 0:
                 bullet.remove_from_sprite_lists()
+        self.process_enemy_bullets()
 
     def update_big_boss(self):
         for enemy in self.enemies.bigBoss_list:
@@ -92,17 +98,17 @@ class MyGame(arcade.Window):
     def on_key_press(self, key, modifiers):
         
         if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            self.player.change_x = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+            self.player.change_x = PLAYER_MOVEMENT_SPEED
 
         elif key == arcade.key.SPACE:
             if len(self.player.player_bullet_list) < 3:
 
                 bullet = arcade.Sprite(":resources:/images/space_shooter/laserRed01.png", scale=0.5)
                 bullet.change_y = +PLAYER_BULLET_SPEED
-                bullet.center_x = self.player_sprite.center_x
-                bullet.bottom = self.player_sprite.top
+                bullet.center_x = self.player.center_x
+                bullet.bottom = self.player.top
 
                 # Add the bullet to the appropriate lists
 
@@ -113,9 +119,9 @@ class MyGame(arcade.Window):
     def on_key_release(self, key, modifiers):
         
         if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = 0
+            self.player.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = 0
+            self.player.change_x = 0
 
 
     def process_player_bullets(self):
@@ -129,11 +135,13 @@ class MyGame(arcade.Window):
             hit_list = arcade.check_for_collision_with_list(bullet, self.enemies.bigBoss_list)
             if hit_list:
                 bullet.remove_from_sprite_lists()
-                for boss in hit_list:
-                    self.enemies.bigBossHP -= 1
-                    if self.enemies.bigBossHP <= 0:
-                        boss.remove_from_sprite_lists()
-                continue
+
+                if len(self.enemies.enemy_list) ==0:
+                    for boss in hit_list:
+                        self.enemies.bigBossHP -= 1
+                        if self.enemies.bigBossHP <= 0:
+                            boss.remove_from_sprite_lists()
+                    continue
 
             # Check collision with enemies
             hit_list = arcade.check_for_collision_with_list(bullet, self.enemies.enemy_list)
@@ -147,21 +155,24 @@ class MyGame(arcade.Window):
                 bullet.remove_from_sprite_lists()
 
 
+        if len(self.enemies.enemy_list) == 0 and len(self.enemies.bigBoss_list) == 0:
+            print("You Win!")
+            game_win = GameWin()
+            self.window.show_view(game_win)
+
 
     def process_enemy_bullets(self):
         self.enemies.enemy_bullet_list.update()
 
-        # Loop through each bullet
         for bullet in self.enemies.enemy_bullet_list:
-
-            # Check collision with Boss
-            hit_list = arcade.check_for_collision_with_list(bullet, self.player.player_list)
-            if hit_list:
+            if arcade.check_for_collision(bullet, self.player):
                 bullet.remove_from_sprite_lists()
-                for enemy in hit_list:
-                    enemy.remove_from_sprite_lists()
-
-
+                self.player.health -= 1
+                print(f"Player hit! Health left: {self.player.health}")
+                if self.player.health <= 0:
+                    print("Game Over!")
+                    game_over = GameOver()
+                    self.window.show_view(game_over)
 
     def allow_enemies_to_fire(self):
 
@@ -179,7 +190,14 @@ class MyGame(arcade.Window):
             if random.randrange(chance) == 0 and enemy.center_x not in x_spawn:
                 self.create_enemy_bullet(enemy)
                 x_spawn.append(enemy.center_x)
-
+        
+    
+        if len(self.enemies.enemy_list) == 0:
+            chance = 30
+            for boss in self.enemies.bigBoss_list:
+                if random.randrange(chance) == 0 and boss.center_x not in x_spawn:
+                    self.create_enemy_bullet(boss)
+                    x_spawn.append(boss.center_x)
 
     def create_enemy_bullet(self, enemy):
         bullet = arcade.Sprite(":resources:/images/space_shooter/meteorGrey_small1.png", scale=0.5)
@@ -189,21 +207,86 @@ class MyGame(arcade.Window):
         bullet.top = enemy.bottom
         self.enemies.enemy_bullet_list.append(bullet)
 
-        if len(self.enemies.enemy_list) <= 0:
-            bossBullet = arcade.Sprite(":resources:/images/space_shooter/meteorGrey_big3.png", scale=0.5)
+        if len(self.enemies.enemy_list) == 0:
+            bossBullet = arcade.Sprite(":resources:/images/space_shooter/meteorGrey_big3.png", scale=1)
             bossBullet.angle = 0
             bossBullet.change_y = -ENEMY_BULLET_SPEED + 2
             bossBullet.center_x = enemy.center_x
             bossBullet.top = enemy.bottom
-            self.enemies.enemy_bullet_list.append(bullet)
+            self.enemies.enemy_bullet_list.append(bossBullet)
+
+class GameOver(arcade.View):
+
+    def __init__(self):
+       super().__init__()
+
+    def on_show_view(self):
+
+        self.window.background_color = arcade.color.BLACK
+
+    def on_draw(self):
+
+        self.clear()
+        arcade.draw_text(
+            "Game Over",
+            start_x=SCREEN_WIDTH/2, 
+            start_y=360, 
+            color=arcade.color.WHITE, 
+            font_size=60, 
+            anchor_x="center")
+
+        arcade.draw_text(
+            "Click to restart",
+            start_x=SCREEN_WIDTH / 2,
+            start_y=300,
+            color=arcade.color.WHITE,
+            font_size=24,
+            anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = MyGame()
+        game_view.setup()
+        self.window.show_view(game_view)
+
+class GameWin(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+
+    def on_show_view(self):
+        self.window.background_color = arcade.color.DARK_BLUE_GRAY
+
+    def on_draw(self):
+
+        self.clear()
+        arcade.draw_text(
+            "You WIN!!",
+            start_x=SCREEN_WIDTH/2, 
+            start_y=360, 
+            color=arcade.color.WHITE, 
+            font_size=60, 
+            anchor_x="center")
+
+        arcade.draw_text(
+            "Click to Play Again",
+            start_x=SCREEN_WIDTH / 2,
+            start_y=300,
+            color=arcade.color.WHITE,
+            font_size=24,
+            anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = MyGame()
+        game_view.setup()
+        self.window.show_view(game_view)
 
 
 def main():
-    game = MyGame()
-    game.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game_view = MyGame()
+    game_view.setup()
+    window.show_view(game_view)
     arcade.run()
 
 if __name__ == "__main__":
     main()
-
-
--- player.py
+++ player.py
@@ -1,14 +1,12 @@
 import arcade
 
-class Player():
+class Player(arcade.Sprite):
     def __init__(self):
-        #super().__init__(":resources:/images/space_shooter/playerShip1_blue.png", scale=0.5)
-        self.player = arcade.Sprite(":resources:/images/space_shooter/playerShip1_blue.png", scale=0.5)
+        super().__init__(":resources:/images/space_shooter/playerShip1_blue.png", scale=0.5)
         self.center_x = 680
         self.center_y = 50
-        self.player_list = arcade.SpriteList()
-        self.player_list.append(self.player)
 
-        self.player_list = arcade.SpriteList()
+        self.health = 1
+
 
         self.player_bullet_list = arcade.SpriteList()
